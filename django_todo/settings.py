6dcc68cd80bc7e301c8676cc3e2f174cbae8fec5
Leonardo Simeone <leo_sime@yahoo.com>
2023-02-21 12:56:58 +0000

    Set up enviroment variables & automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['leo-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
